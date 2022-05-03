1cd76eb61803703ffe9274f2b9b003bfc0728ca6
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2022-05-02 21:07:40 -0600

    Typos

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////
-//This Twitter/Discord client is not secure and was not originally designed for public use, if amything goes wrong it is on you. Uncomment the next line of code to continue.
+//This Twitter/Discord client is not secure and was not originally designed for public use, if anything goes wrong it is on you. Uncomment the next line of code to continue.
 //var check = 1;
 //////////////////////////////////
 var Discord = require('discord.io');
@@ -25,8 +25,8 @@ bot.connect();
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
 console.log(message);
-if (userID == '0000000000') { //Add the userID of your Discord account here to prevent random people from sendimg tweets to your Twitter page.
-if (check === 1) { //This is some old code to help with verifying stuff, not really needed anymore.
+if (userID == '0000000000') { //Add the userID of your Discord account here to prevent random people from sending tweets to your Twitter page.
+if (check === 1) { //This is some old code to help with verifying stuff, though it's not really needed anymore...
 bot.sendMessage({
 to: channelID,
 message: 'Tweeted!',
